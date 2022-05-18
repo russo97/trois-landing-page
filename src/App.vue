472675d0bd7c18f6e78b7c25044986756b6d74ca
Wanderson Silva <wanderson@boldcomunicacao.com.br>
2022-05-18 15:12:57 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    I'm built with {{ tool }}
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'App',
+
+    data () {
+      return {
+        tool: 'VueJS'
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
